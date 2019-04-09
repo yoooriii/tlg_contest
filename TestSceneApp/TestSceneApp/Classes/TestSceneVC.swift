363823c3b47d164b9ad9b48e3f80bff40c760ad3
leonid lokhmatov <Yurii.Lokhmatov@excelian.com>
2019-04-09 13:04:00 +0300

    using stroke shader in sprite kit

--- TestSceneApp/TestSceneApp/Classes/TestSceneVC.swift
+++ TestSceneApp/TestSceneApp/Classes/TestSceneVC.swift
@@ -11,21 +11,37 @@ import QuartzCore
 import SceneKit
 import SpriteKit
 
+
 class TestSceneVC: UIViewController {
     @IBOutlet var skView:SKView!
+    private var contentsCreated = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createContents()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private func createContents() {
+        // do it once
+        if contentsCreated { return }
+        contentsCreated = true
 
         let scene = SKScene(size: skView.bounds.size)
+        /* Modify the SKScene's actual size to exactly match the SKView. */
+        scene.scaleMode = .resizeFill
+        scene.delegate = self
         // Set the scene coordinates (0, 0) to the center of the screen.
         scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         skView.presentScene(scene)
         scene.backgroundColor = .gray
-
-        let shape = createShape()
+        
+        let shape = createTargetShape()
         scene.addChild(shape)
-
+        
         let shape2 = createShape2()
         scene.addChild(shape2)
     }
@@ -42,8 +58,31 @@ class TestSceneVC: UIViewController {
         ball.fillColor = .blue
         ball.strokeColor = .white
         ball.glowWidth = 5
-
+        
         return ball
+    }
+    
+    func createTargetShape() -> SKShapeNode {
+        let path = CGMutablePath()
+        path.addArc(center: CGPoint.zero,
+                    radius: 50,
+                    startAngle: 0,
+                    endAngle: CGFloat.pi * 2,
+                    clockwise: true)
+        let node = SKShapeNode(path: path)
+        node.lineWidth = 1
+        node.fillColor = .clear
+        node.isAntialiased = false
+        node.strokeColor = .red // it doesn't work id strokeShader in use
+        
+        let dashedShader = SKShader(source: "void main() {" +
+            "int stripe = int(u_path_length) / 50;" +
+            "int h = int(v_path_distance) / stripe % 2;" +
+            "gl_FragColor = (0==h) ? float4(0, 0, 1, 1) : float4(0, 1, 0, 1);" +
+            "}")
+        node.strokeShader = dashedShader
+
+        return node
     }
 
     func createShape2() -> SKShapeNode {
@@ -91,4 +130,17 @@ class TestSceneVC: UIViewController {
     @IBAction func changeValueAction(_ sender: UISlider) {
 
     }
+}
+
+extension TestSceneVC: SKSceneDelegate {
+    //    func update(_ currentTime: TimeInterval, for scene: SKScene)
+    //
+    //    func didEvaluateActions(for scene: SKScene)
+    //
+    //    func didSimulatePhysics(for scene: SKScene)
+    //
+    //
+    //    func didApplyConstraints(for scene: SKScene)
+    //
+    //    func didFinishUpdate(for scene: SKScene)
 }
